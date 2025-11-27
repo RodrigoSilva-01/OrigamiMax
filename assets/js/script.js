4da5313e547c6f0bc2ab7073d88d28ed778f868f
RodrigoSilva <rodrigosilva71638@gmail.com>
2025-11-27 14:50:44 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,12 +8,3 @@ function carrossel() {
 }
 
 carrossel(); 
-
-
-// MENU HAMBÚRGUER
-const menuBtn = document.getElementById("menu-btn");
-const nav = document.getElementById("nav");
-
-menuBtn.addEventListener("click", () => {
-    nav.classList.toggle("show");
-});
